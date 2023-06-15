674e3627ff2fba322a9a7b43fa742cc9d1e186e9
Dmitry Stonie <dmitrystonie@gmail.com>
2023-06-15 15:31:32 +0700

    Начал писать поле и ядро игры

---
+++ minesweeper/src/main/java/minesweeper/observerPattern/Observable.java
@@ -0,0 +1,16 @@
+package minesweeper.observerPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Observable {
+    List<Observer> observers = new ArrayList<>();
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+    public void notifyObserver() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
